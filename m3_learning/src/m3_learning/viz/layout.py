16f11a95e28a8fc382af94626de31ce02eb62acc
Yichen Guo <yig319@lehigh.edu>
2023-05-22 13:19:09 -0400

    fix label

--- m3_learning/src/m3_learning/viz/layout.py
+++ m3_learning/src/m3_learning/viz/layout.py
@@ -1,6 +1,9 @@
 from matplotlib.patches import ConnectionPatch
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.transforms as transforms
+from itertools import product
+from matplotlib.text import Annotation
 import numpy as np
 import os
 import torch
@@ -18,12 +21,50 @@ import string
 Path = path.Path
 PathPatch = patches.PathPatch
 
+
+def subfigures(nrows, ncols, size=(1.25, 1.25), gaps=(.8, .33), figsize=None, **kwargs):
+
+    if figsize is None:
+        figsize = (size[0]*ncols + gaps[0]*ncols, size[1]*nrows+gaps[1]*nrows)
+
+    # create a new figure with a size of 6x6 inches
+    fig = plt.figure(figsize=figsize)
+
+    ax = []
+
+    for i, j in product(range(nrows), range(ncols)):
+        rvalue = (nrows-1) - j
+        # create the first axis with absolute position (1 inch, 1 inch) and size (2 inches, 2 inches)
+        pos1 = [(size[0]*rvalue + gaps[0]*rvalue)/figsize[0], (size[1]*i + gaps[1]*i)/figsize[1],
+                size[0]/figsize[0], size[1]/figsize[1]]  # transforms.Bbox.from_bounds()
+        ax.append(fig.add_axes(pos1))
+
+    ax.reverse()
+
+    return fig, ax
+
+
+def add_text_to_figure(fig, text, text_position_in_inches, **kwargs):
+
+    # Get the figure size in inches and dpi
+    fig_size_inches = fig.get_size_inches()
+    fig_dpi = fig.get_dpi()
+
+    # Convert the desired text position in inches to a relative position (0 to 1)
+    text_position_relative = (
+        text_position_in_inches[0] / fig_size_inches[0], text_position_in_inches[1] / fig_size_inches[1])
+
+    # Add the text to the figure with the calculated relative position
+    fig.text(text_position_relative[0],
+             text_position_relative[1], text, **kwargs)
+
+
 def add_box(axs, pos, **kwargs):
-    
-    
+
     xmin, ymin, xmax, ymax = pos
     rect = patches.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin, **kwargs)
     axs.add_patch(rect)
+
 
 def inset_connector(fig, ax1, ax2, coord1=None, coord2=None, **kwargs):
     if coord1 is None:
@@ -42,13 +83,12 @@ def inset_connector(fig, ax1, ax2, coord1=None, coord2=None, **kwargs):
         coord2_l2 = (coord2_xlim[0], coord2_ylim[1])
         coord2 = [coord2_l1, coord2_l2]
 
-    
     for p1, p2 in zip(coord1, coord2):
-        
+
         # Create a connection between the two points
         con = ConnectionPatch(xyA=p1, xyB=p2,
-                            coordsA=ax1.transData, coordsB=ax2.transData, **kwargs)
-        
+                              coordsA=ax1.transData, coordsB=ax2.transData, **kwargs)
+
         # Add the connection to the plot
         fig.add_artist(con)
 
@@ -247,27 +287,16 @@ def combine_lines(*args):
     return lines, labels
 
 
-def labelfigs(
-    axes, number, style="wb", loc="br", string_add="", size=8, text_pos="center"
-):
-    """Function that labels the figures
+def labelfigs(axes, number=None, style="wb",
+              loc="tl", string_add="", size=8,
+              text_pos="center", inset_fraction=0.15):
 
-    Args:
-        axes (axes object): axes to label
-        number (int): number value of the label in letters
-        style (str, optional): sets the style of the label. Defaults to 'wb'.
-        loc (str, optional): sets the location of the label. Defaults to 'br'.
-        string_add (str, optional): Adds a prefix string. Defaults to ''.
-        size (int, optional): sets the font size. Defaults to 14.
-        text_pos (str, optional): sets the position of the text. Defaults to 'center'.
-
-    Raises:
-        ValueError: The string provided for the style is not valid
-    """
+    # initializes an empty string
+    text = ""
 
     # Sets up various color options
     formatting_key = {
-        "wb": dict(color="w", linewidth=1.5),
+        "wb": dict(color="w", linewidth=.75),
         "b": dict(color="k", linewidth=0),
         "w": dict(color="w", linewidth=0),
     }
@@ -275,91 +304,49 @@ def labelfigs(
     # Stores the selected option
     formatting = formatting_key[style]
 
-    # finds the position for the label
-    x_min, x_max = axes.get_xlim()
-    y_min, y_max = axes.get_ylim()
-    x_value = 0.08 * (x_max - x_min) + x_min
+    xlim = axes.get_xlim()
+    ylim = axes.get_ylim()
 
-    # Sets the location of the label on the figure
-    if loc == "br":
-        y_value = y_max - 0.15 * (y_max - y_min)
-        x_value = 0.15 * (x_max - x_min) + x_min
-    elif loc == "tr":
-        y_value = y_max - 0.9 * (y_max - y_min)
-        x_value = 0.08 * (x_max - x_min) + x_min
-    elif loc == "bl":
-        y_value = y_max - 0.1 * (y_max - y_min)
-        x_value = x_max - 0.08 * (x_max - x_min)
-    elif loc == "tl":
-        y_value = y_max - 0.9 * (y_max - y_min)
-        x_value = x_max - 0.08 * (x_max - x_min)
-    elif loc == "tm":
-        y_value = y_max - 0.9 * (y_max - y_min)
-        x_value = x_min + (x_max - x_min) / 2
-    elif loc == "bm":
-        y_value = y_max - 0.1 * (y_max - y_min)
-        x_value = x_min + (x_max - x_min) / 2
+    x_inset = (xlim[1] - xlim[0]) * inset_fraction
+    y_inset = (ylim[1] - ylim[0]) * inset_fraction
+
+    if loc == 'tl':
+        x, y = xlim[0] + x_inset, ylim[1] - y_inset
+    elif loc == 'tr':
+        x, y = xlim[1] - x_inset, ylim[1] - y_inset
+    elif loc == 'bl':
+        x, y = xlim[0] + x_inset, ylim[0] + y_inset
+    elif loc == 'br':
+        x, y = xlim[1] - x_inset, ylim[0] + y_inset
+    elif loc == 'ct':
+        x, y = (xlim[0] + xlim[1]) / 2, ylim[1] - y_inset
+    elif loc == 'cb':
+        x, y = (xlim[0] + xlim[1]) / 2, ylim[0] + y_inset
     else:
         raise ValueError(
-            "Unknown string format imported please look at code for acceptable positions"
-        )
+            "Invalid position. Choose from 'tl', 'tr', 'bl', 'br', 'ct', or 'cb'.")
 
-    # adds a custom string
-    if string_add == "":
+    text += string_add
 
-        # Turns to image number into a label
-        if number < 26:
-            axes.text(
-                x_value,
-                y_value,
-                string.ascii_lowercase[number],
-                size=size,
-                weight="bold",
-                ha=text_pos,
-                va="center",
-                color=formatting["color"],
-                path_effects=[
-                    patheffects.withStroke(
-                        linewidth=formatting["linewidth"], foreground="k"
-                    )
-                ],
-            )
+    if number is not None:
+        text += number_to_letters(number)
 
-        # allows for double letter index
-        else:
-            axes.text(
-                x_value,
-                y_value,
-                string.ascii_lowercase[0] +
-                string.ascii_lowercase[number - 26],
-                size=size,
-                weight="bold",
-                ha=text_pos,
-                va="center",
-                color=formatting["color"],
-                path_effects=[
-                    patheffects.withStroke(
-                        linewidth=formatting["linewidth"], foreground="k"
-                    )
-                ],
-            )
-    else:
-        # writes the text to the figure
-        axes.text(
-            x_value,
-            y_value,
-            string_add,
-            size=size,
-            weight="bold",
-            ha=text_pos,
-            va="center",
-            color=formatting["color"],
-            path_effects=[
-                patheffects.withStroke(
-                    linewidth=formatting["linewidth"], foreground="k"
-                )
-            ],
-        )
+    text_ = axes.text(x, y, text, va='center', ha='center',
+                      path_effects=[patheffects.withStroke(
+                          linewidth=formatting["linewidth"], foreground="k")],
+                      color=formatting["color"],
+                      )
+
+    text_.set_zorder(np.inf)
+
+
+def number_to_letters(num):
+    letters = ''
+    while num >= 0:
+        num, remainder = divmod(num, 26)
+        letters = chr(97 + remainder) + letters
+        num -= 1  # decrease num by 1 because we have processed the current digit
+    return letters
 
 
 def scalebar(axes, image_size, scale_size, units="nm", loc="br"):
@@ -481,3 +468,67 @@ def add_scalebar(ax, scalebar_):
     if scalebar_ is not None:
         scalebar(ax, scalebar_['width'], scalebar_[
             'scale length'], units=scalebar_['units'])
+
+
+def get_axis_pos_inches(fig, ax):
+    """gets the position of the axis in inches
+
+    Args:
+        fig (matplotlib.Figure): figure where the plot is located
+        ax (maplotlib.axes): axes on the plot
+
+    Returns:
+        array: the position of the center bottom of the axis in inches
+    """
+
+    # Get the bounding box of the axis in normalized coordinates (relative to the figure)
+    axis_bbox = ax.get_position()
+
+    # Calculate the center bottom point of the axis in normalized coordinates
+    center_bottom_x = axis_bbox.x0 + axis_bbox.width / 2
+    center_bottom_y = axis_bbox.y0
+
+    # Convert the center bottom point from normalized coordinates to display units
+    center_bottom_display = fig.transFigure.transform(
+        (center_bottom_x, center_bottom_y))
+
+    return center_bottom_display/fig.dpi
+
+
+class FigDimConverter:
+    """class to convert between relative and inches dimensions of a figure
+    """
+
+    def __init__(self, figsize):
+        """initializes the class
+
+        Args:
+            figsize (tuple): figure size in inches
+        """
+
+        self.fig_width = figsize[0]
+        self.fig_height = figsize[1]
+
+    def to_inches(self, x):
+        """Converts position from relative to inches
+
+        Args:
+            x (tuple): position in relative coordinates (left, bottom, width, height)
+
+        Returns:
+            tuple: position in inches (left, bottom, width, height)
+        """
+
+        return (x[0] * self.fig_width, x[1] * self.fig_height, x[2] * self.fig_width, x[3] * self.fig_height)
+
+    def to_relative(self, x):
+        """Converts position from inches to relative
+
+        Args:
+            x (tuple): position in inches (left, bottom, width, height)
+
+        Returns:
+            tuple: position in relative coordinates (left, bottom, width, height)
+        """
+
+        return (x[0] / self.fig_width, x[1] / self.fig_height, x[2] / self.fig_width, x[3] / self.fig_height)
