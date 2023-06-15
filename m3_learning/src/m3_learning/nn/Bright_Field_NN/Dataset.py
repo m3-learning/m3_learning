594b8d5adf3717473d5fbda00e72a8cf8cb59b6c
zhangxinqiao314 <zhang.xinqiao314@gmail.com>
2023-06-15 12:32:04 -0400

    tile viewer finished

--- m3_learning/src/m3_learning/nn/Bright_Field_NN/Dataset.py
+++ m3_learning/src/m3_learning/nn/Bright_Field_NN/Dataset.py
@@ -5,14 +5,17 @@ import h5py
 from skimage.color import rgb2gray
 from scipy import ndimage
 from sklearn.preprocessing import MinMaxScaler
+import skimage
 from tqdm import tqdm
 import imageio
 from pprint import pprint
+import time
+import math
 
 # Pycroscopy
 import sidpy
 import pyNSID
-print('sidpy version: ', sidpy.__version__)
+# print('sidpy version: ', sidpy.__version__)
 import pycroscopy as px
 from pycroscopy.image import ImageWindowing
 import dask.array as da
@@ -43,22 +46,30 @@ class Bright_Field_Dataset:
 
     def list_imgs(self):
         l = [file.split('/')[-2] + ' ' + file.split('/')[-1][:-4] for file in self.temps]
-        pprint(l)
+        # pprint(l)
+        return l
 
-    def get_img(self, state, temperature):
+    def get_raw_img(self, state, temperature):
         """_summary_
 
         Args:
-            state (_type_): "Up" or "Down"
+            state (_type_): "Ramp_Up" or "Ramp_Down"
             temperature (_type_): _description_
         """        
-        s = f"{self.path}/Ramp_{state}/{temperature}.png"
+        s = f"{self.path}/{state}/{temperature}.png"
         im = rgb2gray(imageio.imread(s))
         return im
 
 
-    def get_temps(self):
+    def get_index(self):
+        pass
 
+    def get_temps(self):
+        """gets list of paths to files of all temperatures
+
+        Returns:
+            list: paths to files of all temperatures, sorted by order they were taken
+        """
         up = os.listdir(f'{self.path}/Ramp_Up')
         up.sort(key=self.temp)
         up = list(map(lambda x: f'{self.path}/Ramp_Up/'+x, up))
@@ -69,9 +80,67 @@ class Bright_Field_Dataset:
 
         temps = up+down
         return temps
+    
+    def open_combined_h5(self,name):
+        """_summary_
+
+        Args:
+            name (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """        
+        if len(name) > 0:
+            name += '_'
+        combined = self.path +"/" + name + 'preprocessed.h5'
+        return h5py.File(combined,'a')
+
+    def get_window_indx(self,t,a,b,name='',
+                      windows_group = '',
+                      dset_name = '',
+                      logset_name = '',):
+        """gets the scan from temperature idx t and point a,b, on the sample.
+
+        Args:
+            t (_type_): _description_
+            a (_type_): _description_
+            b (_type_): _description_
+            name (str, optional): _description_. Defaults to ''.
+            windows_group (str, optional): _description_. Defaults to ''.
+            dset_name (str, optional): _description_. Defaults to ''.
+            logset_name (str, optional): _description_. Defaults to ''.
+            filtered (bool, optional): _description_. Defaults to True.
+
+        Returns:
+            idx (integer):
+            bbox (list):
+        """        
+        h = self.open_combined_h5(name)
+        windows_group+='windows'
+        dset_name+='windows_data'
+        logset_name+='windows_logdata'
+
+        # try:
+        grp = h[windows_group][logset_name]
+        orig = h['All_filtered']
+        t_,ox,oy = orig.shape
+        _,a_,b_,_,_ = grp.attrs['orig_shape']
+        x_ = int(np.ceil(grp.attrs['window_size_x']/2))
+        y_ = int(np.ceil(grp.attrs['window_size_y']/2))
+        x = x_ + a*grp.attrs['window_step_x']
+        y = x_ + b*grp.attrs['window_step_y']
+
+        idx = t*a_*b_+a_*a+b
+        bbox = [x-x_, x+x_, y-y_, y+y_]
+        return idx, bbox
+
+
+        # except:
+        #     print('Shape mismatch, or you have not generated windows yet')
+
 
     def write_h5(self, c1, c2, step, name=""):
-        """_summary_
+        """Creates h5_file if it doesn't already exist. Crops raw data and create a dataset with and without Gaussian filtering.
 
         Args:
             c1 (_type_): _description_
@@ -79,14 +148,9 @@ class Bright_Field_Dataset:
             step (_type_): _description_
             name (str, optional): _description_. Defaults to "".
         """
-        
-        if len(name) > 0:
-            name += '_'
-
-        combined = self.path +"/" + name + 'preprocessed.h5'
 
         # put the name of the file you would like to use
-        h = h5py.File(combined, 'a')
+        h = self.open_combined_h5(name)
         t_len = len(self.temps)
 
         if 'All' in h.keys():
@@ -113,67 +177,114 @@ class Bright_Field_Dataset:
 
             h_writef[i] = im1
 
+        # if self.verbose:
+        #     print(f'path to file: {combined}')
+
         h.close()
 
-    def write_windows(self,name,window_params={},
-                      windows_group = 'windows',
-                      dset_name = 'windows_data',
-                      logset_name = 'windows_logdata',
+    def write_windows(self,name='',
+                      windows_group = '',
+                      dset_name = '',
+                      logset_name = '',
                       target_size=128,
-                      filter_threshold=5
+                      filter_threshold=5,
+                      overwrite = False,
+                      window_parms={  'fft_mode': 'abs',
+                                        'interpol_factor': 2,
+                                        'mode': 'fft',
+                                        'window_size_x': 128,
+                                        'window_size_y': 128,
+                                        'window_step_x': 32,
+                                        'window_step_y': 32,
+                                        'zoom_factor': 2 }
                   ):
+        """Generate and take tiled windows with applied parameters. 
+        Writes a raw version and version with logarithm and thresholding applied to h5 file
+
+        Args:
+            name (str): name of sample (ex 'Annealed','Oxygen')
+            window_params (dict, optional): parameters to give pycroscopy windowing function. Defaults to {}.
+            windows_group (str, optional): name of the h5 group where window datasets are written. Defaults to 'windows'.
+            dset_name (str, optional): name of the raw h5 dataset. Defaults to 'windows_data'.
+            logset_name (str, optional): name of the log+threshold h5 dataset. Defaults to 'windows_logdata'.
+            target_size (int, optional): how many pixels are in the final log dataset. Defaults to 128.
+            filter_threshold (int, optional): cutoff for log dataset. Defaults to 5.
+            overwrite (bool): whether or not to delete existing window file
+            window_params (dict):
+
+        Returns:
+        returns ImageWindowing object. From pycroscopy.image package
+        """     
+        window_parms['interpol_factor'] = target_size/window_parms['window_size_x']*window_parms['zoom_factor']
+        iw = ImageWindowing(window_parms)
+        h = self.open_combined_h5(name)
+
+        windows_group+='windows'
+        dset_name+='windows_data'
+        logset_name+='windows_logdata'
         
-        iw = ImageWindowing(window_params)
-        
-        combined = self.path +"/" + name + 'preprocessed.h5'
-        h = h5py.File(combined,'a')
+
+        # set windows group
         if windows_group not in h.keys(): 
             h_windows=h.create_group(windows_group)
         h_windows=h[windows_group]
-
-        for key,val in window_params.items(): # write metadata
-            h_windows.attrs[key]=val
         
         for i,img_path in enumerate(tqdm(self.list_imgs())):
-            im_data = sidpy.Dataset.from_array( self.get_img(img_path) )
-            windows_group.MakeWindows(im_data)
+            tic = time.perf_counter()
+            im_dataset = sidpy.Dataset.from_array( 
+                h['All_filtered'][i]
+                    )
+            windows = iw.MakeWindows(im_dataset)
 
-            if f'filler' in h[windows_group]: 
-                del h[windows_group][f'filler']
+            if f'filler' in h[windows_group]: del h[windows_group][f'filler']
             filler = h[windows_group].create_group(f'filler')
-            pyNSID.hdf_io.write_nsid_dataset(windows_group, filler, main_data_name="windows");
 
+            pyNSID.hdf_io.write_nsid_dataset(windows, filler, main_data_name="windows");
             a,b,x,y = h[windows_group]['filler']['windows']['windows'].shape
             data = h[windows_group]['filler']['windows']['windows'][:].reshape(-1,x,y)
 
-            if dset_name not in h[windows_group].keys(): 
+            if overwrite: 
+                if dset_name in h[windows_group].keys(): del h[windows_group][dset_name]
                 d_windows=h[windows_group].create_dataset(dset_name,shape=(self.t_len*a*b,x,y))
-            d_windows=h[windows_group][dset_name]
-            d_windows[i*a*b:(i+1)*a*b] = data
-
-            if logset_name not in h[windows_group].keys(): 
+                if logset_name in h[windows_group].keys(): del h[windows_group][logset_name]
                 logdata= h[windows_group].create_dataset(logset_name,
-                                                         shape=(self.t_len*a*b,1,target_size,target_size),
-                                                         dtype='f4')
+                                                        shape=(self.t_len*a*b,1,target_size,target_size),
+                                                        dtype='f4')
+            
+            # toc = time.perf_counter()
+            # print(tic-toc)
+
+            else:
+                if dset_name not in h[windows_group].keys(): 
+                    d_windows=h[windows_group].create_dataset(dset_name,shape=(self.t_len*a*b,x,y))
+                if logset_name not in h[windows_group].keys(): 
+                    logdata= h[windows_group].create_dataset(logset_name,
+                                                            shape=(self.t_len*a*b,1,target_size,target_size),
+                                                            dtype='f4')            
+            d_windows=h[windows_group][dset_name]
             logdata=h[windows_group][logset_name]
+            
+            d_windows[i*a*b:(i+1)*a*b] = data
             data = data.reshape(-1,1,x,y)
-            data = skimage.transform.resize(data,(a*b,1,target_size,target_size))
+            # data = skimage.transform.resize(data,(a*b,1,target_size,target_size))
             data = np.log(data+1)
             data[data>filter_threshold]=filter_threshold
             logdata[i*a*b:(i+1)*a*b] = data
 
-            if self.verbose: # print shape and 
-                print('shape: ',a,b,target_size,target_size)
-        
-        for key,val in window_params.items(): # write metadata
-            d_windows.attrs[key]=val
-            logdata.attrs[key]=val
+        if self.verbose: # print shape and 
+            print('shape: ',a,b,target_size,target_size)
+            pprint(window_parms)
 
         d_windows.attrs['orig_shape'] = (self.t_len,a,b,x,y)
         logdata.attrs['orig_shape'] = (self.t_len,a,b,target_size,target_size)
         logdata.attrs['filter_threshold'] = filter_threshold
+        for key,val in window_parms.items(): # write metadata
+            d_windows.attrs[key]=val
+            logdata.attrs[key]=val
 
         h.close()
+
+        return iw
 
 
 # class Bright_Field_Dataset:
--- m3_learning/src/m3_learning/nn/Bright_Field_NN/Viz.py
+++ m3_learning/src/m3_learning/nn/Bright_Field_NN/Viz.py
@@ -1,5 +1,15 @@
 import matplotlib.pyplot as plt
 from m3_learning.viz.layout import imagemap, add_scalebar
+from m3_learning.util.file_IO import make_folder
+from m3_learning.viz.layout import layout_fig
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+from tqdm import tqdm
+from os.path import join as pjoin
+from m3_learning.viz.nn import embeddings as embeddings_
 
 class Viz:
 
@@ -23,7 +33,7 @@ class Viz:
 
 
     def view_raw(self, img_name):
-        data = self.dset.get_img(*img_name)
+        data = self.dset.get_raw_img(*img_name)
         fig,axs = plt.subplots(figsize=(1.25,1.25))
 
         imagemap(axs, data, divider_ = True)
@@ -36,10 +46,84 @@ class Viz:
 
         if self.printer is not None:
               self.printer.savefig(fig,
-                                     f'{img_name[0]}_{img_name[1]}_maps', tight_layout=False)
+                                     f'{img_name[0]}_{img_name[1]}_raw', tight_layout=False,showfig=True)
 
 
+    def view_window(self,t,x,y,name,condition,
+                    window_parms={  'fft_mode': 'abs',
+                                    'interpol_factor': 2.3,
+                                    'mode': 'fft',
+                                    'window_size_x': 128,
+                                    'window_size_y': 128,
+                                    'window_step_x': 32,
+                                    'window_step_y': 32,
+                                    'zoom_factor': 2 }):
+        '''
+        plot the filtered image, transform, and window
         
+        Args:
+            x (int): x tile
+            y (int): y tile
+            name (str): name given to preprocessing h5 file, which is named '(name)_preprocessed.h5'
+            condition (str): "Ramp_up' or 'Ramp_Down'
+        '''
+
+        h = self.dset.open_combined_h5(name)
+        image = h['All_filtered']
+
+        # Make grid
+        # fig = plt.figure(figsize=(16,8))
+        fig = plt.figure()
+        gs = fig.add_gridspec(2, 3)
+        axs = []
+        axs.append( fig.add_subplot(gs[:,0:2]) ) # large subplot (2 rows, 2 columns)
+        axs.append( fig.add_subplot(gs[0,2]) )   # small subplot (1st row, 3rd column)
+        axs.append(fig.add_subplot(gs[1,2]))
+
+        idx,bbox = self.dset.get_window_indx(t,x,y,name=name)
+        print(idx,bbox)
+        
+        # grp[idx,0], orig[t,bbox[0]:bbox[1], bbox[2]:bbox[3]]
+        
+        # plot the full image
+        rect = patches.Rectangle((bbox[0],bbox[2]), bbox[1]-bbox[0],bbox[3]-bbox[2],
+                                 linewidth=2, edgecolor='r', facecolor='none')
+        axs[0].set_title('Full Image')
+        axs[0].add_patch(rect)
+        imagemap(axs[0],image[t],colorbars=True)
+        
+        # a1 = axs[0].imshow(image[:])
+        # rect = patches.Rectangle((xi+32,yi+32),64,64,linewidth=2, edgecolor='r', facecolor='none')
+        # divider = make_axes_locatable(axs[0])
+        # cax = divider.append_axes('right', size='5%')
+        # cb=fig.colorbar(a1, cax=cax, orientation='vertical', pad = 0.2)
+        # cb.ax.xaxis.set_ticks_position('top')
+        # cb.ax.xaxis.set_label_position('top')
+
+        axs[1].set_title('FFT tile')
+        imagemap(axs[1],h['windows']['windows_logdata'][idx,0],colorbars=True)
+
+        # a2 = axs[1].imshow((np.log(fft)))
+        # divider = make_axes_locatable(axs[1])
+        # cax = divider.append_axes('right', size='5%')
+        # cb=fig.colorbar(a2, cax=cax, orientation='vertical', pad = 0.2)
+        # cb.ax.xaxis.set_ticks_position('top')
+        # cb.ax.xaxis.set_label_position('top')
+
+        # fft = dask.array.rechunk(fft,-1)
+        # ifft = np.fft.ifft2(fft).real
+        axs[2].set_title('Image tile')
+        imagemap(axs[2],image[t,bbox[0]:bbox[1],bbox[2]:bbox[3]],colorbars=True)
+
+        # a3 = axs[2].imshow(image[xi:xi+64,yi:yi+64],vmax=1.,vmin=0.)
+        # divider = make_axes_locatable(axs[2])
+        # cax = divider.append_axes('right', size='5%')
+        # cb=fig.colorbar(a3, cax=cax, orientation='vertical', pad = 0.2)
+        # cb.ax.xaxis.set_ticks_position('top')
+        # cb.ax.xaxis.set_label_position('top')
+
+        plt.tight_layout()
+        plt.show()
 
 
 # TODO:
