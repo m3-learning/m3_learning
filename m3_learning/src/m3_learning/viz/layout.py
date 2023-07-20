03fc2185464a32afe1e47a27a5682de628e2d2e3
zhangxinqiao314 <zhang.xinqiao314@gmail.com>
2023-07-20 13:08:57 -0400

    np.int --> int

--- m3_learning/src/m3_learning/viz/layout.py
+++ m3_learning/src/m3_learning/viz/layout.py
@@ -369,7 +369,7 @@ def scalebar(axes, image_size, scale_size, units="nm", loc="br"):
     # gets the size of the image
     x_lim, y_lim = axes.get_xlim(), axes.get_ylim()
     x_size, y_size = np.abs(int(np.floor(x_lim[1] - x_lim[0]))), np.abs(
-        np.int(np.floor(y_lim[1] - y_lim[0]))
+        int(np.floor(y_lim[1] - y_lim[0]))
     )
     # computes the fraction of the image for the scalebar
     fract = scale_size / image_size
