0ded7e0f83964b11d29ab887519ccc886fafadab
jagar2 <jca92@drexel.edu>
2024-02-12 11:00:34 -0500

    update inference search

--- m3_learning/src/m3_learning/be/nn.py
+++ m3_learning/src/m3_learning/be/nn.py
@@ -21,69 +21,6 @@ from m3_learning.nn.Fitter1D.Fitter1D import Multiscale1DFitter, Model
 from m3_learning.nn.Fitter1D.Fitter1D import ComplexPostProcessor
 
 
-# def loop_fitting_function_torch(V, y, type='9 parameters'):
-
-#     y = y.type(torch.float64)
-
-#     if(type == '9 parameters'):
-
-#         vector_length = int(len(V) / 2)
-
-
-#         a0 = y[:, 0]
-#         a1 = y[:, 1]
-#         a2 = y[:, 2]
-#         a3 = y[:, 3]
-#         a4 = y[:, 4]
-#         b0 = y[:, 5]
-#         b1 = y[:, 6]
-#         b2 = y[:, 7]
-#         b3 = y[:, 8]
-#         d = 1000
-#         V1 = torch.tensor(V[:vector_length]).cuda()
-#         V2 = torch.tensor(V[vector_length:]).cuda()
-
-#         g1 = (b1 - b0) / 2 * (torch.erf((V1 - a2) * d) + 1) + b0
-#         g2 = (b3 - b2) / 2 * (torch.erf((V2 - a3) * d) + 1) + b2
-
-#         y1 = (g1 * torch.erf((V1 - a2) / g1) + b0) / (b0 + b1)
-#         y2 = (g2 * torch.erf((V2 - a3) / g2) + b2) / (b2 + b3)
-
-#         f1 = a0 + a1 * y1 + a4 * V1
-#         f2 = a0 + a1 * y2 + a4 * V2
-
-#         loop_eval = torch.transpose(torch.cat((f1, f2), axis=0), 1, 0)
-#         return loop_eval
-
-#     elif(type == '13 parameters'):
-#         a1 = y[:, 0]
-#         a2 = y[:, 1]
-#         a3 = y[:, 2]
-#         b1 = y[:, 3]
-#         b2 = y[:, 4]
-#         b3 = y[:, 5]
-#         b4 = y[:, 6]
-#         b5 = y[:, 7]
-#         b6 = y[:, 8]
-#         b7 = y[:, 9]
-#         b8 = y[:, 10]
-#         Au = y[:, 11]
-#         Al = y[:, 12]
-
-#         # See supporting information for more information about the form of this function
-#         S1 = ((b1 + b2) / 2) + ((b2 - b1) / 2) * torch.erf((V - b7) / b5)
-#         S2 = ((b4 + b3) / 2) + ((b3 - b4) / 2) * torch.erf((V - b8) / b6)
-#         Branch1 = (a1 + a2) / 2 + ((a2 - a1) / 2) * \
-#             torch.erf((V - Au) / S1) + a3 * V
-#         Branch2 = (a1 + a2) / 2 + ((a2 - a1) / 2) * \
-#             torch.erf((V - Al) / S2) + a3 * V
-
-#         return torch.squeeze(torch.cat((Branch1, torch.flipud(Branch2)), axis=0))
-#     else:
-#         print('No such parameters')
-#         return None
-
-
 def write_csv(write_CSV,
               noise_level,
               path,
@@ -733,9 +670,6 @@ def batch_training(dataset, optimizers, noise_list, batch_size, epochs, seed, wr
         # instantiate the model
         model = Model(model_, dataset, training=True,
                       model_basename="SHO_Fitter_original_data")
-
-        # # instantiate the model
-        # model = SHO_Model(dataset, training=True, model_basename=model_name)
 
         # fits the model
         model.fit(
